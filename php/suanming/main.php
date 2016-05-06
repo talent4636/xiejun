e4f4c83cfef7293267ef889cdadbae75ea6a3b23
talent4636@gmail.com <talent4636@gmail.com>
2016-05-06 14:02:19 +0800

    add some

---
+++ php/suanming/main.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiejun
+ * Time: 16/4/25 16:38
+ */
+
+/**
+ * author: xiejun
+ *   time: 2016-04-25 18:08:29
+ *   desc: 根据条件获得内容
+ * params: first_name, last_name, birth_date, birth_time
+ * result: array(
+ *    ''
+ * );
+ */
+function suanming($first_name, $last_name, $birth_date, $birth_time){
+    //1. 获取天格
+}
