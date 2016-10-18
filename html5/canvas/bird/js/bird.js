fab6e4d326da03664fa7459d804ef3d1d23a0830
talent4636@gmail.com <talent4636@gmail.com>
2016-10-18 20:00:17 +0800

    障碍物的渲染框架初步完成 TODO 游戏的主逻辑，判断输赢，加分

--- html5/canvas/bird/js/bird.js
+++ html5/canvas/bird/js/bird.js
@@ -7,12 +7,18 @@ var STATIC_G = -1;//重力加速度 G
 var JUMP_IMAGE_CURRENT_HEIGHT = 100;
 var CANVAS_WIDTH = 1000;
 var CANVAS_HEIGHT = 400;
-var FPS = 30;
+var FPS = 30;//刷新率，本js的计算方式有点问题，不准确，取决于浏览器性能~
 var JUMP_ONCE_HEIGHT = 20;//每一次跳跃的高度
+var BACKGROUND_MOVE_SPEED = 1;//背景移动初始速度(可能关联难度？)
+var BACKGROUND_PER_WIDTH = 30;//每一个障碍物的宽度（是否可以做成变化的？提高难度？）
+var CANVAS_TOTAL_BACKGROUD_NUM = 4;//一个场景最多几个障碍物
+var BACKGROUND_DEFAULT_COLOR = "#467500";
 
 var JUMP_ALWAYS_LEFT = CANVAS_WIDTH/2-JUMP_IMAGE_WIDTH/2;
 var mogu_posi_y = JUMP_IMAGE_CURRENT_HEIGHT;
 var speed = 0;//positive:up   negative:down
+var background_up_arr = [];//上面障碍物数组
+var background_down_arr = [];//下面障碍物数组
 var canvas = document.getElementById("canvas");
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
@@ -31,24 +37,69 @@ mogu.src = JUMP_IMAGE_URL;
 init();
 
 var main = function(){
+    //计算出mogu的速度和位置
     speed += STATIC_G;
     mogu_posi_y -= speed*0.5;
-    console.log("now speed:"+speed);
-    if(mogu_posi_y>(CANVAS_HEIGHT-JUMP_IMAGE_HEIGHT)){
+    if(mogu_posi_y>=(CANVAS_HEIGHT-JUMP_IMAGE_HEIGHT)){
         mogu_posi_y = CANVAS_HEIGHT-JUMP_IMAGE_HEIGHT;
         speed = 0;
-        return;
     }
-    if(mogu_posi_y<0){
+    if(mogu_posi_y<=0){
         mogu_posi_y = 0;
         speed = 0;
-        return;
     }
+    //计算出背景的位置、如果需要生成新的随机背景
+    var need_up_num = CANVAS_TOTAL_BACKGROUD_NUM - background_up_arr.length;
+    var need_down_num = CANVAS_TOTAL_BACKGROUD_NUM - background_down_arr.length;
+    var bg_padding_x = 0.25*CANVAS_WIDTH;//障碍物之间的间隔，其实是可以算出来的
+    if(need_up_num>0){//如果有缺失的上障碍物，就生成
+        for(var i=0;i<need_up_num;i++){
+            //background_up_arr.push({h:50,x:CANVAS_WIDTH-(i+1)*(BACKGROUND_PER_WIDTH+20)});
+            background_up_arr.push({h:100,x:CANVAS_WIDTH-bg_padding_x*i});
+        }
+    }
+    if(need_down_num>0){//如果有缺失的上障碍物，就生成
+        for(var j=0;j<need_down_num;j++){
+            background_down_arr.push({h:120,x:CANVAS_WIDTH-bg_padding_x*j});
+        }
+    }
+    //
+    for(var m = 0; m < CANVAS_TOTAL_BACKGROUD_NUM; m++){
+        //移动的过程，是改变障碍物的x坐标
+        background_up_arr[m].x -= BACKGROUND_MOVE_SPEED;
+        background_down_arr[m].x -= BACKGROUND_MOVE_SPEED;
+        if(background_up_arr[m].x<=(0-BACKGROUND_PER_WIDTH)){//左边超出了，但是还是能看到一点边
+            background_up_arr.splice(m,1);
+        }
+        if(background_down_arr[m].x<=(0-BACKGROUND_PER_WIDTH)){
+            background_down_arr.splice(m,1);
+        }
+    }
+    //TODO 游戏的主逻辑，判断输赢，加分
+    //
     drawJump(mogu_posi_y);
 }
 
+//画背景障碍物 (这个方法里面有报错？TODO）
+var drawBackground = function(){
+    for(var i=0; i<CANVAS_TOTAL_BACKGROUD_NUM; i++){
+        //画上面的障碍物
+        ctx.fillStyle = BACKGROUND_DEFAULT_COLOR;
+        ctx.strokeStyle = BACKGROUND_DEFAULT_COLOR;
+        ctx.lineWidth = 2;
+        ctx.fillRect(background_up_arr[i].x,0,BACKGROUND_PER_WIDTH,background_up_arr[i].h);
+        ctx.strokeRect(background_up_arr[i].x,0,BACKGROUND_PER_WIDTH,background_up_arr[i].h);
+        //画下面的障碍物
+        ctx.fillRect(background_down_arr[i].x,(CANVAS_HEIGHT-background_down_arr[i].h),BACKGROUND_PER_WIDTH,background_down_arr[i].h);
+        ctx.strokeRect(background_down_arr[i].x,(CANVAS_HEIGHT-background_down_arr[i].h),BACKGROUND_PER_WIDTH,background_down_arr[i].h);
+        //console.log(background_up_arr[i].x+"|"+0+"|"+BACKGROUND_PER_WIDTH+"|"+background_up_arr[i].h);
+    }
+}
+
+//方法命名有点问题，应该是渲染整个canvas，而不是jump
 var drawJump = function(positionY){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    drawBackground();
     drawInfo();
     ctx.drawImage(
         mogu,
@@ -68,6 +119,7 @@ var keyUp = function(e) {
     var currKey=0,e=e||event;
     currKey=e.keyCode||e.which||e.charCode;
     if(currKey == 32){
+        //如果已经
         speed += (speed>0)?JUMP_ONCE_HEIGHT/2:JUMP_ONCE_HEIGHT;
         //alert("e.keyCode:"+e.keyCode+";e.which:"+e.which+";e.charCode:"+e.charCode);
     }
