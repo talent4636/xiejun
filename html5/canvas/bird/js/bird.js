c1f947d35358e7542e25389779776700f85ff305
talent4636@gmail.com <talent4636@gmail.com>
2016-10-19 11:28:10 +0800

    游戏基础逻辑初步完成，整体还有很多东西要优化。 尤其不能忍，还有报错。现在要出去了，以后写

--- html5/canvas/bird/js/bird.js
+++ html5/canvas/bird/js/bird.js
@@ -13,12 +13,16 @@ var BACKGROUND_MOVE_SPEED = 1;//背景移动初始速度(可能关联难度？)
 var BACKGROUND_PER_WIDTH = 30;//每一个障碍物的宽度（是否可以做成变化的？提高难度？）
 var CANVAS_TOTAL_BACKGROUD_NUM = 4;//一个场景最多几个障碍物
 var BACKGROUND_DEFAULT_COLOR = "#467500";
+var SCORE_NUMBER_PER = 100;//分数每次增加多少
 
 var JUMP_ALWAYS_LEFT = CANVAS_WIDTH/2-JUMP_IMAGE_WIDTH/2;
 var mogu_posi_y = JUMP_IMAGE_CURRENT_HEIGHT;
 var speed = 0;//positive:up   negative:down
 var background_up_arr = [];//上面障碍物数组
 var background_down_arr = [];//下面障碍物数组
+var gameStatus = 0;// 0正在游戏中；1游戏结束；    2暂停（TODO 暂停）
+var currentScore = 0;//目前分数
+
 var canvas = document.getElementById("canvas");
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
@@ -48,7 +52,7 @@ var main = function(){
         mogu_posi_y = 0;
         speed = 0;
     }
-    //计算出背景的位置、如果需要生成新的随机背景
+    //计算出背景的位置、如果需要生成新的背景   TODO：随机背景？
     var need_up_num = CANVAS_TOTAL_BACKGROUD_NUM - background_up_arr.length;
     var need_down_num = CANVAS_TOTAL_BACKGROUD_NUM - background_down_arr.length;
     var bg_padding_x = 0.25*CANVAS_WIDTH;//障碍物之间的间隔，其实是可以算出来的
@@ -73,14 +77,35 @@ var main = function(){
         }
         if(background_down_arr[m].x<=(0-BACKGROUND_PER_WIDTH)){
             background_down_arr.splice(m,1);
+            //有障碍物消失加分；从这里加分，是不是不太科学？
+            currentScore += SCORE_NUMBER_PER;
         }
     }
-    //TODO 游戏的主逻辑，判断输赢，加分
-    //
+    gameMain();
     drawJump(mogu_posi_y);
 }
 
-//画背景障碍物 (这个方法里面有报错？TODO）
+//游戏的主逻辑，判断输赢，加分
+var gameMain = function(){
+    for(var i=0; i<CANVAS_TOTAL_BACKGROUD_NUM; i++){
+        //如果鸟碰到了障碍物，就输
+        //mogu的  x:Middle  y:mogu_posi_y
+        if(background_down_arr[i].x-JUMP_IMAGE_WIDTH < CANVAS_WIDTH/2 && background_down_arr[i].x+JUMP_IMAGE_WIDTH > CANVAS_WIDTH/2){
+            if(mogu_posi_y<=background_up_arr[i].h
+            || mogu_posi_y+JUMP_IMAGE_HEIGHT>=(CANVAS_HEIGHT-background_down_arr[i].h)){
+                gameStatus = 1;
+                //console.log("游戏结束了！");
+            }
+        }
+    }
+}
+
+var gameReset = function(){
+    currentScore = 0;
+    gameStatus = 0;
+}
+
+//画背景障碍物 (TODO:这个方法里面有报错？妈的完成了再改吧）
 var drawBackground = function(){
     for(var i=0; i<CANVAS_TOTAL_BACKGROUD_NUM; i++){
         //画上面的障碍物
@@ -96,8 +121,25 @@ var drawBackground = function(){
     }
 }
 
+//游戏结束时的场景
+var drawGameOver = function(){
+    var fontSize = 40;
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    ctx.font = fontSize+"px consolas";
+    ctx.fillStyle = "red";
+    ctx.fillText("Game Over", CANVAS_WIDTH/2-fontSize*4, CANVAS_HEIGHT/2-fontSize);
+    ctx.fillStyle = "blue";
+    ctx.font = (fontSize*0.5)+"px consolas";
+    ctx.fillText("点击[R]键重新开始", CANVAS_WIDTH/2-fontSize*4, CANVAS_HEIGHT/2);
+}
+
 //方法命名有点问题，应该是渲染整个canvas，而不是jump
 var drawJump = function(positionY){
+    //如果游戏已经结束了
+    if(gameStatus==1){
+        drawGameOver();
+        return;
+    }
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     drawBackground();
     drawInfo();
@@ -114,20 +156,27 @@ var drawJump = function(positionY){
 //tick tock
 setInterval(main, 1000/FPS);
 
-//listen keyboard event [space button]
+//listen keyboard event [space button & R button]
 var keyUp = function(e) {
     var currKey=0,e=e||event;
     currKey=e.keyCode||e.which||e.charCode;
     if(currKey == 32){
-        //如果已经
         speed += (speed>0)?JUMP_ONCE_HEIGHT/2:JUMP_ONCE_HEIGHT;
         //alert("e.keyCode:"+e.keyCode+";e.which:"+e.which+";e.charCode:"+e.charCode);
     }
     //var keyName = String.fromCharCode(currKey);
     //alert("按键码: " + currKey + " 字符: "+ keyName);
+
+    //如果现在是游戏结束状态，点R键盘reset
+    if(gameStatus==1){
+        if(currKey == 82){
+            gameReset();
+        }
+    }
 }
 document.onkeyup = keyUp;
 
+//写出分数和信息
 var drawInfo = function(){
     ctx.font = "10px consolas";
     ctx.fillStyle = "red";
@@ -135,4 +184,8 @@ var drawInfo = function(){
     ctx.fillText("速度:"+speed, right_posi, CANVAS_HEIGHT-50);
     ctx.fillText("位置:"+mogu_posi_y, right_posi, CANVAS_HEIGHT-35);
     ctx.fillText("Author: xiejun@shopex.cn", right_posi-80, CANVAS_HEIGHT-20);
+    //分数写到右上角
+    ctx.font = "20px cosolas";
+    ctx.fillStyle = '#E1E100';
+    ctx.fillText("分数："+currentScore, right_posi, 50);
 }
